5e05fec06946ec5b6cc1cbeeb5b85fca096fe7fb
Himesh Parashar <himeshparashar424@gmail.com>
2024-08-23 23:53:25 +0530

    chore: Refactor Messages.js for improved readability and search functionality

--- db/Messages.js
+++ db/Messages.js
@@ -1,42 +1,51 @@
 import { supabaseClient } from "../db/params.js";
-  
+
 const SenderType = {
-    USER: 'user',
-    ASSISTANT: 'assistant'
+  USER: "user",
+  ASSISTANT: "assistant",
 };
 
 class Messages {
-    constructor(user, chatId) {
-        this.client = supabaseClient.from("messages");
-        this.user = user;
-        this.chatId = chatId;
+  constructor(user, chatId) {
+    this.client = supabaseClient.from("messages");
+    this.user = user;
+    this.chatId = chatId;
+  }
+
+  async getMessages() {
+    const { data, error } = await this.client
+      .select("*")
+      .eq("user_id", this.user.id)
+      .eq("chat_id", this.chatId)
+      .order("created_at");
+
+    if (error) {
+      throw error;
     }
 
-    async getMessages() {
-        const { data, error } = await this.client
-        .select("*")
-        .eq("user_id", this.user.id)
-        .eq("chat_id", this.chatId)
-        .order("created_at")
-    
-        if (error) {
-            throw error;
-        }
-    
-        return data
+    return data;
+  }
+
+  async newMessage(content, sender, isFinal = false, search_needed = false) {
+    const { data, error } = await this.client
+      .insert([
+        {
+          content: content,
+          sender: sender,
+          chat_id: this.chatId,
+          user_id: this.user.id,
+          is_final: isFinal,
+          search_needed: search_needed,
+        },
+      ])
+      .select();
+
+    if (error) {
+      throw error;
     }
-    
-    async newMessage(content, sender, isFinal = false) {
-        const { data, error } = await this.client
-        .insert([{ content: content, sender: sender, chat_id: this.chatId, user_id: this.user.id, is_final: isFinal }])
-        .select();
-    
-        if (error) {
-            throw error;
-        }
-    
-        return data[0]
-    }
+
+    return data[0];
+  }
 }
 
 export { SenderType, Messages };
